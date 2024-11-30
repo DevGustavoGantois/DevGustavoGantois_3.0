4d564116849bd472282a85596128a07ea3884407
Gustavo Gantois <gugagantois@gmail.com>
2024-11-30 16:34:35 -0300

    feat:Creating the data structure in portuguese

---
+++ src/data/languages/pt-br.ts
@@ -0,0 +1,225 @@
+export const navbarData = [
+    { name: "Home" },
+    { name: "Sobre mim" },
+    { name: "Projetos" },
+    { name: "Serviços" },
+    { name: "Depoimentos" },
+    { name: "Experiência" },
+    { name: "FAQ" },
+    { name: "Contato" },
+];
+
+export const homeHero = [
+    {
+        btnText: "Conheça meus trabalhos",
+    },
+];
+
+export const homeAboutData = [
+    {
+        title: "Sobre mim",
+        description:
+            "E aí! Tudo certo? Meu nome é Gustavo Gantois, sou desenvolvedor web freelancer especializado em tecnologias modernas como Next.js, REACT e Tailwind. Sou um entusiasta de inovação e estou sempre em busca de evoluir minhas habilidades e criar projetos de alta qualidade. Além do desenvolvimento, estudo também design UI/UX, o que me permite combinar código e design para criar interfaces funcionais e atraentes. Apaixonado por aprender e melhorar, estou sempre explorando novas formas de agregar valor aos meus projetos e oferecer uma experiência completa aos clientes!",
+        btnTextPrimary: "Certificados",
+        btnTextSecondary: "Download CV",
+        certificates: [
+            { title: "International Certificate", description: "Full Stack Programming", institution: "Infinity School" },
+            { title: "Fundamentos do React", institution: "RocketSeat" },
+            { title: "Carta de Recomendação", institution: "Infinity School" },
+            { title: "Aprofundamento em Hooks", institution: "RocketSeat" },
+            { title: "Fundamentos de Acessibilidade, Guidelines, WCAG, Leitores de tela", institution: "RocketSeat" },
+            { title: "Acessibilidade, modal, requisição HTTP, conexão com API, gerenciamento de estados via contexto performance.", institution: "RocketSeat" },
+            { title: "Masterizando o TailwindCSS", institution: "RocketSeat" },
+            { title: "NWL Pocket Full Stack (Trilha Intermediária)", institution: "RocketSeat" },
+            { title: "Fundamentos em Next.JS", institution: "RocketSeat" },
+            { title: "Introdução a CiberSegurança", institution: "CISCO" },
+            { title: "Fundamentos a CiberSegurança", institution: "CISCO Networking Academy" },
+            { title: "Certificado do Mundo 1 e 2 de Python", instructor: "Gustavo Guanabara" },
+            { title: "Design System", description: "Design | Storybook | Github Actions", institution: "RocketSeat" },
+            { title: "Integrando o Frontend e Backend", institution: "RocketSeat" },
+            { title: "Aprofundando em Next.js", institution: "RocketSeat" },
+            { title: "Especialista em React", institution: "RocketSeat" }
+        ],
+        graduations: [
+            {
+            degree: "Ensino Médio Completo",
+            institution: "Colégio Sacramentinas - BA",
+            year: 2022
+            },
+            {
+            degree: "Cursando Análise e Desenvolvimento de Sistemas",
+            institution: "UNIFACS",
+            semester: "3º semestre",
+            years: "2023 - 2025"
+            },
+            {
+            degree: "Inglês Pré-Intermediário",
+            institution: "Escola Inglês UPTime",
+            location: "Salvador-BA",
+            year: 2023
+            }
+        ]
+    }
+];
+  
+
+export const homeProjectsData = [
+    {
+        pretitle: "Alguns dos meus",
+        title: "Projetos",
+        subtitle: "Desenvolvendo soluções para meus clientes",
+        workDescription: "Etapas de como trabalho!",
+        steps: [
+        "1.Entendo a necessidade do cliente.",
+        "2.Envio do contrato para o desenvolvimento do serviço.",
+        "3.Encaminho o briefing para entender as necessidades de forma mais detalhada.",
+        "4.Criação do Design UI | Design do Website do contratante.",
+        "5.Desenvolvimento do código, após isso haverá entrega do site novinho em folha!",
+        ],
+    },
+];
+
+export const homeServicesData = [{
+    title: "Serviços",
+    services: [
+            {
+                name: "Web Development",
+                description: "Desenvolvimento de websites modernos, rápidos e responsivos, utilizando as melhores tecnologias do mercado."
+            },
+            {
+                name: "Design UI/UX",
+                description: "Criação de interfaces intuitivas e experiências de usuário que garantem a melhor navegação possível."
+            },
+            {
+                name: "Web Design",
+                description: "Design visual atrativo e funcional para websites, alinhado à identidade da sua marca."
+            },
+            {
+                name: "Suporte 24h",
+                description: "Ofereço suporte técnico contínuo para garantir que seu site esteja sempre funcionando perfeitamente."
+            },
+            {
+                name: "Criação de Sistemas",
+                description: "Desenvolvimento de sistemas personalizados para atender às necessidades específicas do seu negócio."
+            }
+    ]
+}];
+
+export const homeTestimonialsData = [{
+    title: "Depoimentos",
+    testimonials: [
+        {
+            name: 'Eduardo Sávio',
+            testimonial: '"Ótimo trabalho! O site está responsivo e com uma ótima experiência de usuário, design excepcional com animações bem leves e reativas como eu esperava. Meus parabéns!"'
+        },
+        {
+            name: 'Matheus Hayashi',
+            testimonial: '"Irmão está muito bom o site, ficou perfeito!"'
+        },
+        {
+            name: 'Samuel Fernandes',
+            testimonial: '"Site ficou muito bom, convence muito bem o cliente de fechar a compra, me poupa muito o trabalho de vender no boca a boca, entregou dentro do prazo e supriu todas as minhas expectativas"'
+        },
+        {
+            name: 'Matheus Caria',
+            description: '"O site ficou muito bom irmão! Está muito lindo de verdade, muito obrigado!"'
+        },
+        {
+            name: 'Maurício Sampaio',
+            description: '"Guga, você é o cara, o site ficou muito bom, você conseguiu transmitir dentro do site exatamente o que eu te pedi."'
+        },
+        {
+            name: 'Thomaz',
+            description: '"Fera demais! Parabéns mesmo, o projeto ficou muito bom!"'
+        }
+    ]
+}];
+
+export const homeExperiencesData = [{
+    title: "Experiências",
+    description: "Algumas das minhas experiências profissionais",
+    experiences: [
+        {
+            title: 'Empresa Novasce',
+            position: 'Estagiário',
+            funcionality: 'Desenvolvedor Front-end | Designer UI',
+            description: 'Responsável pelo desenvolvimento de interfaces (front-end) utilizando frameworks como Next.js e TailwindCSS, aliados à linguagem TypeScript e à biblioteca ShadcnUI. Atualmente, contribuo para a empresa desenvolvendo integralmente o front-end dos projetos e colaborando ativamente na área de UI/UX da empresa. Possuo sólida proficiência em UI Design, utilizando o Figma para criar design systems e projetar telas completas para os sistemas em desenvolvimento.'
+        },
+        {
+            title: 'Infinity School',
+            position: 'Estagiário',
+            funcionality: 'Desenvolvedor Full Stack',
+            description: 'Responsável pelo desenvolvimento full stack, com tecnologias como HTML5, CSS3 com Bootstrap, JavaScript, AJAX, e Django com Python, aplicando Programação Orientada a Objetos (POO) no backend. Contribuí para a empresa no desenvolvimento do aplicativo Infinity App, voltado para o departamento de RH, visando otimizar processos internos e melhorar a eficiência operacional. Experiência em bancos de dados.'
+        },
+        {
+            title: 'Desenvolvedor Web Freelancer',
+            position: 'Autônomo | Freelancer',
+            description: 'Atuo em uma agência digital e também como profissional autônomo na criação de websites, programação, como também nas plataformas WordPress e Shopify. Desenvolvo projetos com foco em design inovador, responsividade e otimização para SEO, aplicando boas práticas como o uso adequado de tags e otimização de imagens, visando melhorar o posicionamento dos sites no Google Search Console. Como web designer, utilizo o Figma para criar layouts de websites e aplicativos móveis, garantindo uma experiência visual coerente e funcional. Além disso, desenvolvo o style guide completo e organizo os arquivos de design em pastas estruturadas, facilitando o entendimento e a continuidade do projeto pelo cliente, transmitindo profissionalismo e eficiência.'
+        },
+    ]
+}];
+
+export const homeTechnologiesData = [{
+    title: "Tecnologias que utilizo",
+    description: "Conheça o meu trabalho como desenvolvedor Web"
+}];
+
+export const homeFAQData = {
+    title: "FAQ",
+    subtitle: "Perguntas Frequentes",
+    questionsAndAnswers: [
+      {
+        question: "Como funciona sua forma proposta e sua forma de Trabalho?",
+        answer: `O processo começa com uma análise detalhada das necessidades do cliente, realizada por meio de uma reunião ou WhatsApp, conforme a preferência do cliente. 
+        Em seguida, envio um briefing para compreender melhor as expectativas e requisitos específicos do projeto.
+        
+        Apresento um contrato formal para estabelecer um compromisso mútuo, garantindo segurança e transparência durante todo o desenvolvimento do projeto.
+        
+        Após a assinatura do contrato, inicio a prototipagem no Figma, mantendo o cliente informado e oferecendo suporte contínuo, disponível 24 horas por dia.
+        
+        Utilizo a plataforma escolhida pelo cliente para garantir que suas preferências e necessidades sejam atendidas ao longo do processo de desenvolvimento.
+        
+        Adoto as melhores práticas de codificação para assegurar um site responsivo, moderno e escalável, focado em desempenho e usabilidade.
+        
+        Entrego o site dentro do prazo acordado, acompanhado de todo o design e documentação necessária, incluindo um Style Guide detalhado.
+        
+        Integro o site ao Google Search Console e implemento estratégias eficazes de SEO para melhorar o ranqueamento e a visibilidade online.`
+      },
+      {
+        question: "Quanto tempo leva para desenvolver um site?",
+        answer: `O tempo de desenvolvimento varia conforme a complexidade do site. Projetos simples podem ser concluídos em algumas semanas, enquanto os mais complexos podem levar meses.`
+      },
+      {
+        question: "Como posso garantir que meu site seja responsivo?",
+        answer: `Utilizo técnicas de design responsivo, como grids, media queries e frameworks como Bootstrap ou Tailwind CSS, para garantir uma excelente experiência em diferentes tamanhos de tela.`
+      },
+      {
+        question: "Quais tipos de site você desenvolve?",
+        answer: `Desenvolvo diversos tipos de sites: Landing Pages, Sites Institucionais, Blogs, Portais, E-commerces e Sites Imersivos, entre outros.`
+      },
+      {
+        question: "Você oferece somente serviços de criação de sites/desenvolvimento web?",
+        answer: `Não! Também ofereço serviços de design, incluindo design de aplicativos móveis, criativos e designs para posts em redes sociais.`
+      },
+      {
+        question: "Você trabalha somente com frontend?",
+        answer: `Atualmente, foco no desenvolvimento de interfaces de usuário (UI) e experiência do usuário (UX). 
+        Não trabalho com bancos de dados ou servidores no momento, mas estou aberto a expandir esses serviços conforme as necessidades dos clientes.`
+      },
+      {
+        question: "Você trabalha com plataformas como WordPress, Shopify ou Wix Studio?",
+        answer: `Sim, trabalho com todas essas plataformas, além de desenvolver projetos dentro de editores de código, oferecendo flexibilidade e personalização.`
+      }
+    ]
+  };
+
+export const homeContactData = [{
+    title: "Contact",
+    description: "Entre em contato para desenvolvermos juntos o seu projeto! Vamos transformar suas ideias em soluções digitais inovadoras, trazendo suas visões para o ambiente online com excelência.",
+    btnTextPrimary: 'Whatsapp',
+    btnTextSecondary: 'Instagram',
+}];
+
+export const copyrightData = [{
+    copyright: '@Copyright. Todos os direitos reservados @devgustavogantois_ 2024'
+}];
