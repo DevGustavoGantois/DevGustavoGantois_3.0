ea04195c74f46f918ad640a0a66cbe7ebd9c9720
Gustavo Gantois <gugagantois@gmail.com>
2024-11-30 21:47:14 -0300

    feat:Setting up AOS animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import { LanguageProvider } from "@/context/language-context";
 import { HomeAbout } from "./s-about";
 import { HomeHero } from "./s-hero";
 import { HomeProjects } from "./s-projects";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    })
+  }, []);
+
   return (
     <>
       <LanguageProvider>
