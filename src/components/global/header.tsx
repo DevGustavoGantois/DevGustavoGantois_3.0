841e2fd1e22d0b5a41ab20be721ec1dd08e918c8
Gustavo Gantois <gugagantois@gmail.com>
2024-11-30 22:03:00 -0300

    feat: Setting up header global component

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,7 +1,15 @@
+import { Navbar } from "./navbar";
+import { NavbarMobile } from "./navbar-mobile";
+
 export function Header() {
     return (
-        <header>
-            
+        <header className="max-w-[1500px] p-6 lg:p-0 mx-auto">
+            <div className="hidden lg:flex justify-center lg:mt-20">
+                <Navbar />
+            </div>
+            <div className="mt-4 lg:hidden">
+                <NavbarMobile />
+            </div>
         </header>
     )
 }
